15c36875e419297c7405b7281a7aa8be0d93fcff
Aaron Paul Lewis <60684646+lewisaaronpaul@users.noreply.github.com>
2025-02-11 00:43:43 -0600

    Music
    First commit

---
+++ static/js/music-list.js
@@ -0,0 +1,482 @@
+let allMusic = [
+    {
+        name: "Belizean Goddess",
+        artist: "Nicki Nicole",
+        img: "Nic",  // This will load static/images/db.jpg
+        src: "special-audio"  // Use actual audio file or silent MP3
+    },
+    {
+        name: "Favorite Song",
+        artist: "Toosii",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Shape of You",
+        artist: "Ed Sheeran",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Essence",
+        artist: "Wizkid ft Tems",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Free Yourself",
+        artist: "Fantasia Barrino",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Good Morning Gorgeous",
+        artist: "Mary J. Blige",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Focus",
+        artist: "H.E.R.",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "ICU",
+        artist: "Coco Jones",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Free Mind",
+        artist: "Tems",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Mientras Me Curo Del Cora",
+        artist: "Karol G",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Slow Down",
+        artist: "Skip Marley & H.E.R.",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "It Kills Me",
+        artist: "Melanie Fiona",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "GLU",
+        artist: "Usher",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Lift Me Up",
+        artist: "Rihanna",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Easy On Me",
+        artist: "Adele",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "For My Hand",
+        artist: "Burna Boy ft. Ed Sheeran",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "As Long As You Love Me",
+        artist: "Justin Bieber",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "If I Ever Fall In Love",
+        artist: "Shai",
+        img: "music-17",
+        src: "music-99"
+    },
+    {
+        name: "Come For  Me",
+        artist: "Alicia Keys",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Damages",
+        artist: "Tems",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Perfect",
+        artist: "Johnny Gill ft Ralph Tresvant",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "Best Part",
+        artist: "Daniel Caesar ft H.E.R.)",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name: "Gangsta Lovin'",
+        artist: "Eve",
+        img: "music-22",
+        src: "music-22"
+    },
+    {
+        name: "Let Me Blow Ya Mind",
+        artist: "Eve ft Gwen Stefani",
+        img: "music-23",
+        src: "music-23"
+    },
+    {
+        name: "When I See U",
+        artist: "Fantasia",
+        img: "music-24",
+        src: "music-24"
+    },
+    {
+        name: "Hrs & Hrs",
+        artist: "Muni Long",
+        img: "music-25",
+        src: "music-25"
+    },
+    {
+        name: "Gotta Move On",
+        artist: "Toni Braxton ft H.E.R",
+        img: "music-26",
+        src: "music-26"
+    },
+    {
+        name: "Need U Bad",
+        artist: "Jazmine Sullivan",
+        img: "music-27",
+        src: "music-27"
+    },
+    {
+        name: "Heaven Sent",
+        artist: "Keyshia Cole",
+        img: "music-28",
+        src: "music-28"
+    },
+    {
+        name: "Unforgettable",
+        artist: "French Montana ft Swae Lee",
+        img: "music-29",
+        src: "music-29"
+    },
+    {
+        name: "Go Crazy",
+        artist: "Chris Brown & Young Thug",
+        img: "music-30",
+        src: "music-30"
+    },
+    {
+        name: "All Mine",
+        artist: "Ralph Tresvant ft Johnny Gill",
+        img: "music-31",
+        src: "music-31"
+    },
+    {
+        name: "Oops (Oh My)",
+        artist: "Tweet ft Missy Elliott",
+        img: "music-32",
+        src: "music-32"
+    },
+    {
+        name: "Envolver",
+        artist: "Anitta",
+        img: "music-33",
+        src: "music-33"
+    },
+    {
+        name: "The Climb",
+        artist: "Miley Cyrus",
+        img: "music-34",
+        src: "music-34"
+    },
+    {
+        name: "Hurt Me So Good",
+        artist: "Jazmine Sullivan",
+        img: "music-35",
+        src: "music-35"
+    },
+    {
+        name: "Scars To Your Beautiful",
+        artist: "Alessia Cara",
+        img: "music-36",
+        src: "music-36"
+    },
+    {
+        name: "You",
+        artist: "Jesse Powell",
+        img: "music-37",
+        src: "music-37"
+    },
+    {
+        name: "Anything For You",
+        artist: "Ledisi",
+        img: "music-38",
+        src: "music-38"
+    },
+    {
+        name: "Special",
+        artist: "Lizzo",
+        img: "music-39",
+        src: "music-39"
+    },
+    {
+        name: "Waves",
+        artist: "Mr. Probz",
+        img: "music-40",
+        src: "music-40"
+    },
+    {
+        name: "The Journey",
+        artist: "H.E.R.",
+        img: "music-41",
+        src: "music-41"
+    },
+    {
+        name: "Forever",
+        artist: "R. Kelly",
+        img: "music-42",
+        src: "music-42"
+    },
+    {
+        name: "Customer",
+        artist: "Raheem Devaughn",
+        img: "music-43",
+        src: "music-43"
+    },
+    {
+        name: "Here We Go (Uh Oh)",
+        artist: "Coco Jones",
+        img: "music-44",
+        src: "music-44"
+    },
+    {
+        name: "Sensational",
+        artist: "Chris Brown",
+        img: "music-45",
+        src: "music-45"
+    },
+    {
+        name: "Rush",
+        artist: "Ayra Starr",
+        img: "music-46",
+        src: "music-46"
+    },
+    {
+        name: "Last Heartbreak",
+        artist: "Ayra Starr ft. Giveon",
+        img: "music-47",
+        src: "music-47"
+    },
+    {
+        name: "Commas",
+        artist: "Ayra Starr",
+        img: "music-48",
+        src: "music-48"
+    },
+    {
+        name: "Call Out My Name",
+        artist: "The Weeknd",
+        img: "music-49",
+        src: "music-49"
+    },
+    {
+        name: "Calm Down",
+        artist: "Rema & Selena Gomez",
+        img: "music-50",
+        src: "music-50"
+    },
+    {
+        name: "Leave Me Alone",
+        artist: "Calypso Rose ft. Machel Montano",
+        img: "music-51",
+        src: "music-51"
+    },
+    {
+        name: "Lucy",
+        artist: "Destra",
+        img: "music-52",
+        src: "music-52"
+    },
+    {
+        name: "Who The Hell Is Kim",
+        artist: "Destra",
+        img: "music-53",
+        src: "music-53"
+    },
+    {
+        name: "Call My Name",
+        artist: "Destra",
+        img: "music-54",
+        src: "music-54"
+    },
+    {
+        name: "Fallin",
+        artist: "Kes",
+        img: "music-55",
+        src: "music-55"
+    },
+    {
+        name: "Hello",
+        artist: "Kes",
+        img: "music-56",
+        src: "music-56"
+    },
+    {
+        name: "Mr. Fete",
+        artist: "Machel Montano",
+        img: "music-57",
+        src: "music-57"
+    },
+    {
+        name: "Just A Little Bit",
+        artist: "Destra",
+        img: "music-58",
+        src: "music-58"
+    },
+    {
+        name: "Ola",
+        artist: "Olatunji",
+        img: "music-59",
+        src: "music-59"
+    },
+    {
+        name: "Mind My Business",
+        artist: "Patrice Roberts",
+        img: "music-60",
+        src: "music-60"
+    },
+    {
+        name: "Old & Grey",
+        artist: "Patrice Roberts",
+        img: "music-61",
+        src: "music-61"
+    },
+    {
+        name: "Phenomenal",
+        artist: "Benjai",
+        img: "music-62",
+        src: "music-62"
+    },
+    {
+        name: "It's Plenty",
+        artist: "Burna Boy",
+        img: "music-63",
+        src: "music-63"
+    },
+    {
+        name: "Something About Ya",
+        artist: "J. Howell",
+        img: "music-64",
+        src: "music-64"
+    },
+    {
+        name: "My Everything",
+        artist: "J. Howell",
+        img: "music-65",
+        src: "music-65"
+    },
+    {
+        name: "Horas y Horas",
+        artist: "Muni Long",
+        img: "music-66",
+        src: "music-66"
+    },
+    {
+        name: "Made For Me",
+        artist: "Muni Long",
+        img: "music-67",
+        src: "music-67"
+    },
+    {
+        name: "Ruined Me",
+        artist: "Muni Long",
+        img: "music-68",
+        src: "music-68"
+    },
+    {
+        name: "Back To Your Place",
+        artist: "October London",
+        img: "music-69",
+        src: "music-69"
+    },
+    {
+        name: "Whoa",
+        artist: "Snoh Aalegra",
+        img: "music-70",
+        src: "music-70"
+    },
+    {
+        name: "Lose Control",
+        artist: "Teddy Swims",
+        img: "music-71",
+        src: "music-71"
+    },
+    {
+        name: "Love Me JeJe",
+        artist: "Tems",
+        img: "music-72",
+        src: "music-72"
+    },
+    {
+        name: "Me & U",
+        artist: "Tems",
+        img: "music-73",
+        src: "music-73"
+    },
+    {
+        name: "Higher",
+        artist: "Tems",
+        img: "music-74",
+        src: "music-74"
+    },
+    {
+        name: "Push 2 Start",
+        artist: "Tyla",
+        img: "music-75",
+        src: "music-75"
+    },
+    {
+        name: "Water",
+        artist: "Tyla",
+        img: "music-76",
+        src: "music-76"
+    },
+    {
+        name: "On My Mama",
+        artist: "Victoria Monét",
+        img: "music-77",
+        src: "music-77"
+    },
+    {
+        name: "SOS (Sex on Sight)",
+        artist: "Victoria Monét ft. Usher",
+        img: "music-78",
+        src: "music-78"
+    },
+    {
+        name: "Do You Want To",
+        artist: "Xscape",
+        img: "music-79",
+        src: "music-79"
+    }
+]
